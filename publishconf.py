f700bd94cae7081dde1b4de2c8fd46886bc3cc71
Joe Hamman <jhamman1@uw.edu>
2015-08-27 14:49:10 -0700

    update after a while away

--- publishconf.py
+++ publishconf.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
 
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
 import sys
-sys.path.append('.')
+sys.path.append(os.curdir)
 from pelicanconf import *
 
-# keep the .com address to properly find disqus comments
-SITEURL = 'http://jhamman.github.io'
+SITEURL = 'http://www.hydro.washington.edu/~jhamman/hydro-logic'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ATOM = '~jhamman/hydro-logic/atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
-
 GOOGLE_ANALYTICS = 'UA-6032956-6'
-DISQUS_SITENAME = 'hydrologic'
+DISQUS_SITENAME = 'hydro-logic'
